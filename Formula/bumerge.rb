89864295ce8994499eff53741b9f0ae74ca2c56a
Nikita Karamov <me@kytta.dev>
2024-09-11 11:30:09 +0200

    Depend on Python 3.12

--- Formula/bumerge.rb
+++ Formula/bumerge.rb
@@ -13,7 +13,7 @@ class Bumerge < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux: "01950b8a31bf31c7d135ee80a646252b0ad9027e7e2215194edeab7311110dd6"
   end
 
-  depends_on "python"
+  depends_on "python@3.12"
 
   resource "ruamel-yaml" do
     url "https://files.pythonhosted.org/packages/29/81/4dfc17eb6ebb1aac314a3eb863c1325b907863a1b8b1382cdffcb6ac0ed9/ruamel.yaml-0.18.6.tar.gz"
